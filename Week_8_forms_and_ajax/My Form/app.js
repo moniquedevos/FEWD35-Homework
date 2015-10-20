78b51e2a7396328dceaba8c6beaea11c5dde48e8
Monique de Vos <stellatrigger@gmail.com>
2015-10-19 19:47:01 -0700

    minor changes made in class

--- Week_8_forms_and_ajax/My Form/app.js
+++ Week_8_forms_and_ajax/My Form/app.js
@@ -3,18 +3,21 @@ $(document).on("submit", "form", function(event) {
 	event.preventDefault ();
 
 	var formInfo = {
-		firstname: $("input[name='first-name']").val(),
-		lastname: $("input[name='last-name']").val(),
+		firstname: $("input[name='firstname']").val(),
+		lastname: $("input[name='lastname']").val(),
 		email: $("input[name='email']").val(),
 		address: $("input[name='address']").val(),
+		address2: $("input[name='address2']").val(),
 		city: $("input[name='city']").val(),
 		state: $("select[name='state']").val(),
 		country: $("select[name='country']").val(),
 
 	};
+	console.log(formInfo);
+
 	for (key in formInfo) {
-		if (formInfo [key]=== "") {
-			alert("Please fill out all fields!");
+		if (formInfo [key]=== "" && key !=="address2") {
+			alert("Please fill-in all fields!");
 			return false;
 		}
 	}
